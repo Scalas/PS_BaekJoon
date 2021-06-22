7b90ba83ef13a182577c486b9177c716689b8fc9
Scala <scala0114@gmail.com>
2021-06-22 15:17:21 +0900

    review & modified +1 problem

--- solutions/sol1037.py
+++ solutions/sol1037.py
@@ -1,13 +1,9 @@
 import sys
 
-input = sys.stdin.readline
+input = sys.stdin.read
 
 
 def sol1037():
-    input()
-    div = list(map(int, input().split()))
-    div.sort()
-
-    print(div[0]*div[-1])
-
-
+    n, *nums = map(int, input().split())
+    nums.sort()
+    print(nums[0] * nums[-1])
