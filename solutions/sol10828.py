1f153d62dec41c6b4546dfd323987fbabf73915d
Scala <scala0114@gmail.com>
2021-06-27 20:07:54 +0900

    review & modify +1 problem

--- solutions/sol10828.py
+++ solutions/sol10828.py
@@ -1,42 +1,24 @@
-import sys
+from sys import stdin
 
-input = sys.stdin.readline
-answer = []
-param = 0
-
-
-def push(st):
-    global param
-    st.append(param)
-
-
-def pop(st):
-    answer.append(-1 if not st else st.pop())
-
-
-def size(st):
-    answer.append(len(st))
-
-
-def empty(st):
-    answer.append(0 if st else 1)
-
-
-def top(st):
-    answer.append(-1 if not st else st[-1])
+input = stdin.readline
 
 
 # 10828 스택
 # 간단한 스택을 구현하는 문제
-# dictionary 로 명령어에 해당하는 함수를 매칭하여 구현
 def sol10828():
     n = int(input())
-    global param
-    st = []
-    func = {'push': push, 'pop': pop, 'size': size, 'empty': empty, 'top': top}
+    stack = []
+    answer = []
     for _ in range(n):
         cmd = input().split()
-        if (len(cmd) == 2):
-            param = int(cmd[1])
-        func[cmd[0]](st)
-    print(*answer, sep='\n')
+        if len(cmd) > 1:
+            stack.append(cmd[1])
+        elif cmd[0][0] == 'p':
+            answer.append(stack.pop() if stack else '-1')
+        elif cmd[0][0] == 't':
+            answer.append(stack[-1] if stack else '-1')
+        elif cmd[0][0] == 's':
+            answer.append(str(len(stack)))
+        elif cmd[0][0] == 'e':
+            answer.append('0' if stack else '1')
+    print('\n'.join(answer))
