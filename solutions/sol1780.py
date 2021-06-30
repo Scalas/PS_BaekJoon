3f51204f784c1e3d181aaff5a6e67229d7574135
Scala <scala0114@gmail.com>
2021-06-30 17:10:28 +0900

    review & modify +1 problem

--- solutions/sol1780.py
+++ solutions/sol1780.py
@@ -3,7 +3,7 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 paper = [list(map(int, input().split())) for _ in range(n)]
-answer = [0, 0, 0]
+res = [0, 0, 0]
 
 
 # 1780 종이의 개수
@@ -11,47 +11,38 @@ answer = [0, 0, 0]
 # 데이터의 크기가 커져서 기존에 푼 방식대로 종이를 O(N^2)으로 탐색하여 잘라야할지 판단하면 시간초과가 발생
 # 판단하지 않고 종이를 잘라서 재귀호출한 뒤 그 결과를 토대로 자를 필요가 있었는지 판정
 def sol1780():
-    cut((0, n), (0, n))
-    print(*answer, sep='\n')
+    cut(0, 0, n)
+    print('\n'.join(map(str, res)))
 
 
-def cut(r, c):
-    global paper
-    global answer
-
+def cut(x, y, l):
     # 1칸짜리 종이라면 해당 종이 색의 카운트를 1 증가 후 종이의 색을 반환
-    if (r[1] - r[0] == 1):
-        answer[paper[r[0]][c[0]] + 1] += 1
-        return paper[r[0]][c[0]]
+    if l == 1:
+        res[paper[x][y]+1] += 1
+        return paper[x][y]
 
     # 종이를 9조각으로 분할, 결과를 저장
-    pieces = []
+    nl = l // 3
+    p = []
     for i in range(3):
         for j in range(3):
-            nr = (r[0] + i * ((r[1] - r[0]) // 3), r[0] + (i + 1) * ((r[1] - r[0]) // 3))
-            nc = (c[0] + j * ((c[1] - c[0]) // 3), c[0] + (j + 1) * ((c[1] - c[0]) // 3))
-            pieces.append(cut(nr, nc))
+            p.append(cut(x+nl*i, y+nl*j, nl))
 
     # 비교를 위한 첫번쨰 조각의 결과
-    start = pieces[0]
+    check = p[0]
 
     # 만약 첫번째 조각이 모두 같은색인 종이가 아니었다면
-    # 현재 종이도 모두 같은색이 아님(-2 반환)
-    if (start == -2):
+    if check == -2:
         return -2
 
-    for piece in pieces[1:]:
+    for i in p[1:]:
         # 하나의 조각라도 다른 결과가 나왔다면 현재 종이는 모두 같은색이 아님(-2 반환)
-        if (piece != start):
+        if i != check:
             return -2
 
     # 모두 같은색인 종이였다면 분할하지 않아도 될 종이를 분할하여 추가로 카운트된 종이의 수만큼 카운트를 감소
     # 1로 끝날 카운트가 9칸치 상승했기 때문에 -8
-    answer[start + 1] -= 8
+    res[check+1] -= 8
 
     # 종이 색을 반환
-    return start
-
-# C++ 등의 언어였다면 단순히 먼저 조건을 확인한 후 분할하여도 시간초과는 되지 않았을 것으로 추정
-# 파이썬이 문제풀이에 유리한 언어이긴 하지만 이런 경우에 느린 속도가 발목을 잡을 수 있음
-# 보조언어로 C++ 등을 익혀둘것
+    return check
