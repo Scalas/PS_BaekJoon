7c1edbfa9f766c507ebdc2fa009cbc4fcb6b7b32
Scala <scala0114@gmail.com>
2021-07-03 11:23:45 +0900

    review & modify +1 problem

--- solutions/sol12015.py
+++ solutions/sol12015.py
@@ -10,26 +10,21 @@ input = sys.stdin.readline
 
 # 직접 이분탐색을 구현한 풀이
 def sol12015():
-    input()
-    A = map(int, input().split())
-    seq = [next(A)]
-    for a in A:
-        # A 수열의 숫자들을 순차적으로 참조, seq의 마지막 숫자보다 크다면 append (최장 증가 수열을 연장)
-        if (a > seq[-1]):
-            seq.append(a)
-
-        # seq 내의 각 숫자가 작을수록 보다 큰 증가수열의 길이를 얻어낼 수 있음
-        # append 되지 못한 숫자는 seq 내에서 자신보다 크거나 같은 수 중에 가장 작은 수를 대체
+    n, f, *seq = map(int, sys.stdin.read().split())
+    lis = [f]
+    for num in seq:
+        if num > lis[-1]:
+            lis.append(num)
         else:
-            s, e = 0, len(seq) - 1
-            while (s <= e):
+            s, e = 0, len(lis) - 1
+            while s <= e:
                 m = (s + e) // 2
-                if (seq[m] >= a):
-                    e = m - 1
-                else:
+                if lis[m] < num:
                     s = m + 1
-            seq[min(e + 1, len(seq) - 1)] = a
-    print(len(seq))
+                else:
+                    e = m - 1
+            lis[e + 1] = num
+    print(len(lis))
 
 
 # bisect 모듈을 활용한 풀이
