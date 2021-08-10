533871bbb7ae84b474c03033f8022d6a79a895ba
Scala <scala0114@gmail.com>
2021-08-10 13:49:19 +0900

    review & modify +1 problem

--- solutions/sol2981.py
+++ solutions/sol2981.py
@@ -11,22 +11,21 @@ input = sys.stdin.read
 # 약수를 구할때는 최대공약수의 제곱근까지만 탐색, 나눈 m값과 몫이 모두 약수가 된다
 # 1을 포함하지 않기때문에 최대공약수 자체도 따로 추가해줘야한다
 def sol2981():
-    n, *nums = map(int, input().split())
-    g, *diff = [abs(nums[i + 1] - nums[i]) for i in range(n - 1)]
+    n, *seq = map(int, input().split())
+    g, *diff = [abs(seq[i] - seq[i - 1]) for i in range(1, n)]
     for d in diff:
         g = gcd(g, d)
-
-    answer = {g}
-    for i in range(2, int(g ** .5) + 1):
+    res = {g}
+    for i in range(2, int(g**.5) + 1):
         if g % i == 0:
-            answer.add(i)
-            answer.add(g // i)
-    print(' '.join(map(str, sorted(answer))))
+            res.add(i)
+            res.add(g // i)
+    return ' '.join(map(str, sorted(res)))
 
 
-def gcd(n, m):
-    if n < m:
-        n, m = m, n
-    while m != 0:
-        n, m = m, n % m
-    return n
+def gcd(a, b):
+    if a < b:
+        a, b = b, a
+    while b != 0:
+        a, b = b, a % b
+    return a
