f58eff5b36895b326238a150b08df0c993dfecae
Scala <scala0114@gmail.com>
2021-08-14 11:26:49 +0900

    review & modify +1 problem

--- solutions/sol1966.py
+++ solutions/sol1966.py
@@ -12,17 +12,27 @@ input = sys.stdin.readline
 
 # 리스트 연산으로 구현
 def sol1966():
-    answer = []
-    for t in range(int(input())):
+    res = []
+    # 테스트케이스 루프
+    for _ in range(int(input())):
         n, m = map(int, input().split())
-        docs = [*map(int, input().split())]
-        turn = 1
-        while True:
-            i = docs.index(max(docs))
-            if i == m:
+
+        # 문서의 중요도와 초기 인덱스를 묶어서 리스트형태로 저장
+        docs = list(zip(map(int, input().split()), range(n)))
+
+        # 프린트 시작
+        for i in range(1, n + 1):
+            # 가장 우선도가 높은 문서중 가장 앞에 있는 문서를 출력
+            # 문서의 초기인덱스가 m과 같다면 res에 답을 append하고 다음 테스트케이스로
+            p = max(docs, key=lambda x: x[0])
+            if p[1] == m:
+                res.append(i)
                 break
-            docs = docs[i + 1:] + docs[:i]
-            m = m - i - 1 if i < m else len(docs) - i + m
-            turn += 1
-        answer.append(str(turn))
-    print('\n'.join(answer))
+
+            # 실제로는 출력할 문서가 나올때까지 모든 문서를 하나하나 큐의 맨 뒤로 보내야하지만
+            # 빠르고 간결하게 처리하기 위해 리스트 슬라이싱을 사용하여 같은 결과를 낸다.
+            # 출력할 문서를 기준으로 좌우를 슬라이싱한뒤 순서를 바꾸어 이어붙인다.
+            pi = docs.index(p)
+            docs = docs[pi + 1:] + docs[:pi]
+
+    return '\n'.join(map(str, res))
