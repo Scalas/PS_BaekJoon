80f33a22262da1a3960dac91c3f6509a045639d7
Scala <scala0114@gmail.com>
2021-08-17 15:53:04 +0900

    review & modify +1 problem

--- solutions/sol1654.py
+++ solutions/sol1654.py
@@ -7,14 +7,13 @@ from sys import stdin, stdout
 # 조건에 맞는 선의 길이 m을 분할정복으로 탐색, 조건에 맞을경우 그보다 큰쪽의 범위에서 재탐색을 반복하여
 # 조건을 만족하는 가장 큰 m을 찾을 수 있다
 def sol1654():
-    k, n, *lan = map(int, stdin.read().split())
-    s, e = 1, sum(lan) // n
-    answer = 0
+    k, n, *lans = map(int, input().split())
+
+    s, e = 1, sum(lans) // n
     while s <= e:
         m = (s + e) // 2
-        if sum([l // m for l in lan]) >= n:
-            answer = m
-            s = m + 1
-        else:
+        if sum([lan // m for lan in lans]) < n:
             e = m - 1
-    print(answer)
+        else:
+            s = m + 1
+    return s - 1
