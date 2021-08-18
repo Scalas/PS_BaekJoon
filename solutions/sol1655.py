2814220988099724cc32d77d4725fe9a75a95cc9
Scala <scala0114@gmail.com>
2021-08-18 16:19:41 +0900

    review & modify +1 problem

--- solutions/sol1655.py
+++ solutions/sol1655.py
@@ -1,33 +1,34 @@
 import sys
-import heapq
+from heapq import heappush, heappop
 
 
 # 1655 가운데를 말해요
 # 힙을 활용한 중간값 찾기 문제
-
-# 최소힙, 최대힙을 동시에 사용하면 해결 가능하다
-# 중간값을 찾기 위해서 최소힙과 최대힙의 크기의 차는 2이상이 되어선 안되며
-# 최대힙의 top 은 최소힙의 top 보다 작아야한다.
-# 먼저 최대힙이 비어있다면 최대힙에 숫자를 넣고
-# 그 이후부터는 최대힙의 top 보다 작거나 같은 수는 최대힙에, 큰 수는 최소힙에 삽입한다
-# 둘의 크기 차이가 2가 될 경우 큰쪽을 하나 pop 하여 작은 쪽으로 push 하는 것으로 균형을 맞춘다
-# 하나의 숫자가 삽입될 때마다 중간값은 두 힙중 크기가 큰쪽의 top, 두 힙의 크기가 같다면 최대힙의 top 이 된다
 def sol1655():
     maxq, minq = [], []
     n, *nums = map(int, sys.stdin.read().split())
     answer = []
     for num in nums:
+        # 최대 힙이 비어있거나 수가 최대 힙의 최댓값 이상이라면 최대힙에 heappush
         if not maxq or num <= -maxq[0]:
-            heapq.heappush(maxq, -num)
+            heappush(maxq, -num)
+        # 그렇지 않다면 최소 힙에 heappush
         else:
-            heapq.heappush(minq, num)
+            heappush(minq, num)
+
         diff = len(maxq) - len(minq)
+        # 두 힙의 크기차이가 1보다 커지면 균형을 맞추고 크기차이를 0으로 세팅
+        # maxq가 minq보다 2 크다면 maxq에서 원소 하나를 제거하여 minq에 넣어준다
         if diff == 2:
-            heapq.heappush(minq, -heapq.heappop(maxq))
+            heappush(minq, -heappop(maxq))
             diff = 0
+        # minq가 maxq보다 2 크다면 minq에서 원소 하나를 제거하여 maxq에 넣어준다
         elif diff == -2:
-            heapq.heappush(maxq, -heapq.heappop(minq))
+            heappush(maxq, -heappop(minq))
             diff = 0
 
-        answer.append(str(-maxq[0] if diff >= 0 else minq[0]))
-    print('\n'.join(answer))
+        # 두 힙의 크기차이가 없거나 maxq쪽이 크면 maxq의 최댓값이 중간값
+        # 그렇지 않다면 minq의 최솟값이 중간값
+        answer.append(-maxq[0] if diff >= 0 else minq[0])
+
+    return '\n'.join(map(str, answer))
