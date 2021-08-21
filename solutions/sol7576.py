6e01d84d0d36e72d8e31d8d2c2ab56843767aa76
Scala <scala0114@gmail.com>
2021-08-21 12:26:22 +0900

    review & modify +1 problem

--- solutions/sol7576.py
+++ solutions/sol7576.py
@@ -1,35 +1,57 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-direction = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
 
 # 7576 토마토
 # bfs로 토마토 익히기 문제
 def sol7576():
     m, n = map(int, input().split())
+    # 토마토 상태
     tomato = [input().split() for _ in range(n)]
 
-    q = deque()
-    cnt = 0
+    # BFS 큐
+    q = []
+
+    # 익지 않은 토마토 수
+    tc = 0
+
     for i in range(n):
         for j in range(m):
-            if tomato[i][j] == '1':
-                q.append((i, j, 0))
-            elif tomato[i][j] == '0':
-                cnt += 1
+            # 익지 않은 토마토가 있다면 익지 않은 토마토 갯수 증가
+            if tomato[i][j] == '0':
+                tc += 1
+            # 익은 토마토가 있다면 좌표를 BFS 큐에 삽입
+            elif tomato[i][j] == '1':
+                q.append((i, j))
 
-    answer = 0
-    while q and cnt:
-        r, c, t = q.popleft()
-        t += 1
-        for d in direction:
-            nr, nc = r + d[0], c + d[1]
-            if 0 <= nr < n and 0 <= nc < m and tomato[nr][nc]=='0':
-                tomato[nr][nc] = '1'
-                cnt -= 1
-                answer = t
-                q.append((nr, nc, t))
+    # 익지않은 토마토가 없다면 0을 반환 - 이미 다 익어있음
+    if tc == 0:
+        return 0
 
-    print(-1 if cnt else answer)
+    # 익는데 필요한 날짜
+    res = 0
+
+    while q:
+        nq = []
+        for r, c in q:
+            # 익은 토마토에 인접한 토마토 중 익지 않은것이 있는 경우
+            # 토마토를 익히고 익지 않은 토마토 수를 감소시킨다
+            # 만약 익지 않은 토마토의 갯수가 0이된다면 지난 일수 res에 당일을 포함하여 res+1 반환
+            # 익은 토마토의 좌표를 큐에 삽입
+            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                if 0 <= nr < n and 0 <= nc < m and tomato[nr][nc] == '0':
+                    tomato[nr][nc] = '1'
+                    tc -= 1
+                    if tc == 0:
+                        return res + 1
+                    nq.append((nr, nc))
+        # 큐 교체
+        q = nq
+
+        # 일수 + 1
+        res += 1
+
+    # 큐를 빠져나올 때 까지 익지 않은 토마토 수가 0이 되지 않았다면
+    # 모든 토마토를 익힐 수 없는 배치이기 때문에 -1 반환
+    return -1
