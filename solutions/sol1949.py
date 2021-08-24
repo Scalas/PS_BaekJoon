9788d0e3b679c0e8e42f68f413e6e0e4a6ea4090
Scala <scala0114@gmail.com>
2021-08-24 15:04:46 +0900

    solve +1 problem

---
+++ solutions/sol1949.py
@@ -0,0 +1,39 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+
+input = sys.stdin.readline
+
+
+def sol1949():
+    # 노드의 갯수
+    n = int(input())
+
+    # 각 노드의 가중치
+    w = [0] + list(map(int, input().split()))
+
+    # 트리 구성
+    g = [[] for _ in range(n + 1)]
+    for _ in range(n - 1):
+        u, v = map(int, input().split())
+        g[u].append(v)
+        g[v].append(u)
+
+    # 탐색함수
+    def dfs(pre, cur):
+        # 현재 노드가 우수마을에 포함될지 여부에 따른 우수마을 주민 수 합의 최댓값
+        res = [0, w[cur]]
+
+        # 현재 노드의 자식노드들에 대해
+        for nxt in g[cur]:
+            if nxt != pre:
+                inc, ninc = dfs(cur, nxt)
+                # 현재 노드가 우수마을이 아니라면 다음 노드는 우수마을이거나 우수마을이 아닐 수 있다.
+                res[0] += max(inc, ninc)
+
+                # 현재 노드가 우수마을이라면 다음 노드는 우수마을이 아니어야 한다.
+                res[1] += inc
+        return res
+
+    # 우수 마을 주민 수 합의 최댓값 반환
+    return max(dfs(0, 1))
