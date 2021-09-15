e251147506e52fb67726262870ab59a145b14fe4
Scala <scala0114@gmail.com>
2021-09-15 14:17:58 +0900

    review & modify +1 problem

--- solutions/sol2252.py
+++ solutions/sol2252.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 # 2252 줄 세우기
 # 학생의 수와 키를 비교한 결과가 주어졌을 때 학생들을 키순서로 정렬하는 문제
 # 위상정렬을 사용하여 간단하게 해결 가능한 문제이다.
-def sol2252():
+def sol2252_1():
     # 학생의 수 n, 비교 결과의 수 m
     n, m = map(int, input().split())
 
@@ -41,3 +41,46 @@ def sol2252():
 
     # 출력 형식에 맞춰 정렬 결과를 반환
     return ' '.join(map(str, answer))
+
+
+# 추가: dfs를 활용하여 위상정렬을 구현한 코드
+def sol2252_2():
+    # 학생 수 n, 키 비교 결과 수 m
+    n, m = map(int, input().split())
+
+    # 그래프, 진입 차수 테이블 생성
+    # bfs 형태의 구현과 달리 진입 차수 테이블은 생성이후 수정할 일이 없음
+    g = [[] for _ in range(n + 1)]
+    degree = [0] * (n + 1)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        g[a].append(b)
+        degree[b] += 1
+
+    # dfs를 활용한 위상 정렬 구현
+    def dfs(d):
+        nonlocal answer
+        # 현재 노드 방문처리
+        visit[d] = True
+
+        # 아직 방문하지 않은 자식노드 방문
+        for child in g[d]:
+            if not visit[child]:
+                dfs(child)
+
+        # 탐색이 종료된 노드를 answer에 삽입
+        answer.append(d)
+
+    # 방문여부 리스트
+    visit = [False] * (n + 1)
+
+    # 정렬 결과 리스트
+    answer = []
+
+    # 처음부터 진입 차수가 0인 노드로부터 탐색 시작
+    for i in range(1, n + 1):
+        if not degree[i]:
+            dfs(i)
+
+    # 정렬 결과 리스트를 뒤집어 출력 형식에 맞춰 반환
+    return ' '.join(map(str, answer[::-1]))
